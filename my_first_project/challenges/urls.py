46cf3d1c687bebbf4a55959508ed588a81478940
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-02 15:42:16 +0530

    Add monthly paths to urlpatterns

--- my_first_project/challenges/urls.py
+++ my_first_project/challenges/urls.py
@@ -2,4 +2,17 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("january", views.index)]
+urlpatterns = [
+    path("january", views.index),
+    path("february", views.index),
+    path("march", views.index),
+    path("april", views.index),
+    path("may", views.index),
+    path("june", views.index),
+    path("july", views.index),
+    path("august", views.index),
+    path("september", views.index),
+    path("october", views.index),
+    path("november", views.index),
+    path("december", views.index),
+]
