9b95ff3d6f7776cfedb12aaebe1beb1f82438af1
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-02 23:46:08 +0530

    Refactor views and add challenges app to settings

--- my_first_project/challenges/views.py
+++ my_first_project/challenges/views.py
@@ -3,6 +3,7 @@ from urllib import response
 from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 monthly_challenges = {
     "january": "In january do not eating out meat challenge",
@@ -20,68 +21,16 @@ monthly_challenges = {
 }
 
 
-# @ Define the URL patterns for the monthly challenges (ststic URL patterns)
-def january(request):
-    return HttpResponse("In january do not eating out meat challenge")
-
-
-def february(request):
-    return HttpResponse("In february Walk for at least 20 minutes every day challenge")
-
-
-def march(request):
-    return HttpResponse(
-        "In march Learn Django for at least 20 minutes every day challenge"
-    )
-
-
-def april(request):
-    return HttpResponse("In april dance for at least 20 minutes every day challenge")
-
-
-def may(request):
-    return HttpResponse("In may exercise for at least 20 minutes every day challenge")
-
-
-def june(request):
-    return HttpResponse("In june Read for at least 20 minutes every day challenge")
-
-
-def july(request):
-    return HttpResponse(
-        "In july Learn NEXT.js for at least 20 minutes every day challenge"
-    )
-
-
-def august(request):
-    return HttpResponse("In august eat only meat challenge")
-
-
-def september(request):
-    return HttpResponse("In september eat only vegetables challenge")
-
-
-def october(request):
-    return HttpResponse("In october eat only fruits challenge")
-
-
-def november(request):
-    return HttpResponse("In november eat only nuts challenge")
-
-
-def december(request):
-
-    return HttpResponse("In december eat only seeds challenge")
-
-
 # @ Define the URL patterns for the monthly challenges (dynamic URL patterns)
 def monthly_challenge(request, month):
-    try:
-        challenge_text = monthly_challenges[month]
-        response_data = f"<h1>{challenge_text}</h1>"
-        return HttpResponse(response_data)
-    except:
-        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
+    # try:
+    challenge_text = monthly_challenges[month]
+    response_data = render_to_string("challenges/challenge.html")
+    return HttpResponse(response_data)
+
+
+# except:
+#     return HttpResponseNotFound("<h1>This month is not supported!</h1>")
 
 
 # @ Define a function to handle requests for monthly challenges by number
@@ -89,18 +38,12 @@ def monthly_challenges_by_number(
     request,
     number,
 ):
-    # Create a list of sorted months from the keys of the monthly_challenges dictionary
     sorted_months = list(monthly_challenges.keys())
-
-    # Check if the provided number is greater than the number of months
     if number > len(sorted_months):
-        # If the number is invalid, return a 404 Not Found response
         return HttpResponseNotFound("Invalid month")
 
-    # Calculate the index of the redirect month in the sorted_months list
     redirect_month = sorted_months[number - 1]
 
-    # Redirect the user to the URL for the corresponding monthly challenge
     redirect_path = reverse("month-challenge", args=[redirect_month])
 
     return HttpResponseRedirect(redirect_path)
--- my_first_project/my_first_project/settings.py
+++ my_first_project/my_first_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+import django
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "challenges",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,9 @@ ROOT_URLCONF = "my_first_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            # BASE_DIR / "challenges" / "templates"
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
