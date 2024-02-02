caaa742c9d0773593185bb197a750d6c2ccc1579
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-02 15:20:14 +0530

    learned basic routing

--- my_first_project/my_first_project/urls.py
+++ my_first_project/my_first_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("challenges/", include("challenges.urls")),
 ]
