872deb190c9619111945c7027f1bbda2bd716c34
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-02 23:03:25 +0530

    Add view for all months in challenges

--- my_first_project/my_first_project/urls.py
+++ my_first_project/my_first_project/urls.py
@@ -18,8 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("challenges/", include("challenges.urls")),
+    path("challenges/", views.all_months),
 ]
---
+++ my_first_project/my_first_project/views.py
@@ -0,0 +1,12 @@
+from challenges.views import monthly_challenges
+from django.http import HttpResponse
+
+
+def all_months(request):
+    months_list = list(monthly_challenges.keys())
+    response = "<ul>"
+    for month in months_list:
+        month_path = f"/challenges/{month}"
+        response += f'<li><a href="{month_path}">{month.capitalize()}</a></li>'
+    response += "</ul>"
+    return HttpResponse(response)
