ed06f31090a4568dccb87e7c8a04952946af10ff
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-02 15:48:10 +0530

    Update challenge URLs and views

--- my_first_project/challenges/urls.py
+++ my_first_project/challenges/urls.py
@@ -3,16 +3,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january", views.index),
-    path("february", views.index),
-    path("march", views.index),
-    path("april", views.index),
-    path("may", views.index),
-    path("june", views.index),
-    path("july", views.index),
-    path("august", views.index),
-    path("september", views.index),
-    path("october", views.index),
-    path("november", views.index),
-    path("december", views.index),
+    path("january", views.january),
+    path("february", views.february),
+    path("march", views.march),
+    path("april", views.april),
+    path("may", views.may),
+    path("june", views.june),
+    path("july", views.july),
+    path("august", views.august),
+    path("september", views.september),
+    path("october", views.october),
+    path("november", views.november),
+    path("december", views.december),
 ]
--- my_first_project/challenges/views.py
+++ my_first_project/challenges/views.py
@@ -4,5 +4,54 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Hello from Ankur Halder!")
+def january(request):
+    return HttpResponse("In january do not eating out meat challenge")
+
+
+def february(request):
+    return HttpResponse("In february Walk for at least 20 minutes every day challenge")
+
+
+def march(request):
+    return HttpResponse(
+        "In march Learn Django for at least 20 minutes every day challenge"
+    )
+
+
+def april(request):
+    return HttpResponse("In april dance for at least 20 minutes every day challenge")
+
+
+def may(request):
+    return HttpResponse("In may exercise for at least 20 minutes every day challenge")
+
+
+def june(request):
+    return HttpResponse("In june Read for at least 20 minutes every day challenge")
+
+
+def july(request):
+    return HttpResponse(
+        "In july Learn NEXT.js for at least 20 minutes every day challenge"
+    )
+
+
+def august(request):
+    return HttpResponse("In august eat only meat challenge")
+
+
+def september(request):
+    return HttpResponse("In september eat only vegetables challenge")
+
+
+def october(request):
+    return HttpResponse("In october eat only fruits challenge")
+
+
+def november(request):
+    return HttpResponse("In november eat only nuts challenge")
+
+
+def december(request):
+
+    return HttpResponse("In december eat only seeds challenge")
