1e4c31ae533677b2a77f74ea82fddd27428eb779
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-03 00:11:51 +0530

    Update challenge.html and views.py

--- my_first_project/challenges/views.py
+++ my_first_project/challenges/views.py
@@ -24,12 +24,17 @@ monthly_challenges = {
 # @ Define the URL patterns for the monthly challenges (dynamic URL patterns)
 def monthly_challenge(request, month):
     try:
-        # challenge_text = monthly_challenges[month]
+        challenge_text = monthly_challenges[month]
         # response_data = render_to_string("challenges/challenge.html")
         # return HttpResponse(response_data)
         return render(
             request,
             "challenges/challenge.html",
+            {
+                "text": challenge_text,
+                "month": month.capitalize(),
+                "request": request,
+            },
         )
     except:
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")
