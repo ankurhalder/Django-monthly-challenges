0cff37e7faba6a1a76641cefdd3e27c5e3c9215e
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-03 11:41:46 +0530

    bug fi9xed with new djlint formatter

--- my_first_project/challenges/views.py
+++ my_first_project/challenges/views.py
@@ -23,21 +23,23 @@ monthly_challenges = {
 
 # @ Define the URL patterns for the monthly challenges (dynamic URL patterns)
 def monthly_challenge(request, month):
-    try:
-        challenge_text = monthly_challenges[month]
-        # response_data = render_to_string("challenges/challenge.html")
-        # return HttpResponse(response_data)
-        return render(
-            request,
-            "challenges/challenge.html",
-            {
-                "text": challenge_text,
-                "month": month.capitalize(),
-                "request": request,
-            },
-        )
-    except:
-        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
+    # try:
+    challenge_text = monthly_challenges[month]
+    # response_data = render_to_string("challenges/challenge.html")
+    # return HttpResponse(response_data)
+    return render(
+        request,
+        "challenges/challenge.html",
+        {
+            "text": challenge_text,
+            "month": month.capitalize(),
+            "request": request,
+        },
+    )
+
+
+# except:
+#     return HttpResponseNotFound("<h1>This month is not supported!</h1>")
 
 
 # @ Define a function to handle requests for monthly challenges by number
