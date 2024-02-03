9592b79b795bdb3ef347b4a4d1b81ecad5e8de62
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-03 10:11:58 +0530

    Refactor challenge and index templates

--- my_first_project/my_first_project/settings.py
+++ my_first_project/my_first_project/settings.py
@@ -59,6 +59,8 @@ TEMPLATES = [
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
             # BASE_DIR / "challenges" / "templates"
+            BASE_DIR
+            / "templates",
         ],
         "APP_DIRS": True,
         "OPTIONS": {
